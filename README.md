ac686f95c292104c3acd4236d13e87cd83c0030d
Darth Arminius <arminpudic@yahoo.co.uk>
2019-10-10 18:41:57 +0100

    Update README & add comments

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# slack-app-talk-template
-A quickstart template of a Slack App used for the AND Digital Slack App Dev Lightning Talk
+# Slack App Talk Template
+
+## A quickstart template of a Slack App used for the AND Digital Slack App Dev Lightning Talk
+
+# Installation
+
+`npm install`
+
+# Running the app
+
+`node index.js`
+
+# Hosting the app
+
+You will need to host the app somewhere public in order to integrate it with your Slack App.
+
+`ngrok` is a great way of locally hosting your app and tunnelling a public URL to your localhost https://ngrok.com/
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const { App } = require('@slack/bolt');
 
+// You can get these tokens from your Slack App configuration page
+// Make sure to never share or publically post your tokens
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+// Simple message api that can listen to a message and respond
+// message is the object containing the message sent and all it's metadata
+// say is a service that can send messages to the chat
 app.message('Hi', async ({ message, say }) => {
   say({
+    // blocks are how you can send Slack UI elements in messages
+    // Check out the Slack Block Kit Builder for a nice WYSIWYG UI that auto generates the JSON for you
     blocks: [
       {
         type: 'section',
@@ -20,17 +27,18 @@ app.message('Hi', async ({ message, say }) => {
             type: 'plain_text',
             text: 'Click Me',
           },
-          action_id: 'button_click_handler',
+          action_id: 'button_click_handler', // This ties the action of clicking the button to a handler
         },
       },
     ],
   });
 });
 
+// Actions are handlers for events ranging from button clicks, to messages and even simply logging in
 app.action('button_click_handler', ({ body, ack, say }) => {
-  ack();
+  ack(); // It is important to acknowledge the event first otherwise it will time out
 
-  say(`You push my buttons <@${body.user.id}>`);
+  say(`You push my buttons <@${body.user.id}>`); // This action sends a message mentioning the user that fired off the event but actions can do many other things
 });
 
 (async () => {
