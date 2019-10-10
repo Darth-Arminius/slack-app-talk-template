fc583d659080d43f618e9f95dba8b6dbd5c1eda8
Darth Arminius <arminpudic@yahoo.co.uk>
2019-10-10 15:02:55 +0100

    Add initial bootstrap files

---
+++ index.js
@@ -0,0 +1,12 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  await app.start(4390);
+
+  console.log('Slack App is running');
+})();
